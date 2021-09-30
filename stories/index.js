5617124699dfeacab787f292c4e92f9b30355680
BelenMaru <bleneee@gmail.com>
2021-09-30 01:33:30 -0400

    Add error components and stories to the repo

--- stories/index.js
+++ stories/index.js
@@ -28,6 +28,8 @@ import Confirm from "components/Appointment/Confirm";
 
 import Status from "components/Appointment/Status";
 
+import Error from "components/Appointment/Error";
+
 // Button stories
 storiesOf("Button", module)
   .addParameters({
@@ -169,4 +171,10 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
-  .add("Status", () => <Status message="Deleting" />);
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  ));
