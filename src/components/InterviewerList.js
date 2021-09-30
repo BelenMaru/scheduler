44edfdba0dcf7f74aed61308d96951c8dd7e7fc7
BelenMaru <bleneee@gmail.com>
2021-09-29 20:07:54 -0400

    Add the interviewer listand styling to the repo

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,4 +2,26 @@ import React from "react";
 
 import classNames from "classnames";
 
-export default fucntion interviewerList
+import "components/InterviewerList.scss";
+
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={props.setInterviewer}
+      selected={props.interviewer === interviewer.id}
+      />
+    );
+  });
+  return (
+    <div className="interviewers">
+      <div className="interviewers__header">Interviewer</div>
+      <div className="interviewers__list">{interviewers}</div>
+    </div>
+  );
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,30 +2,23 @@ import React from "react";
 
 import classNames from "classnames";
 
-
 import "components/InterviewerListItem.scss";
 
-export default function InterviewerListItem(props){
-  console.log(props , 'this is the props')
+export default function InterviewerListItem(props) {
+  // console.log(props , 'this is the props')
 
-  const interviewerClass = classNames ( 
-    "interviewers__item" , {
-    "interviewers__item--selected": props.selected
-    });
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
 
   return (
     <li className={interviewerClass} onClick={props.setInterviewer}>
-    <img 
-      className="interviewers__item-image"
-      src={props.avatar}
-      alt={props.name}
-  />
-    {props.name}
-</li>
-
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
   );
-
 }
-
-
-
